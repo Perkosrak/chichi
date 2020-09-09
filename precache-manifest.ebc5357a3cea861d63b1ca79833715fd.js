050d1a8fbd3e87565b877a41b2e3bfae4238eaec
panikite <ne.isnuje@gmail.com>
2020-09-09 23:54:31 +0300

    Updates

---
+++ precache-manifest.ebc5357a3cea861d63b1ca79833715fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f23e8b297bd0d0a2caf6c0234b95d1",
+    "url": "/chichi/index.html"
+  },
+  {
+    "revision": "549d23095f4ece040dcd",
+    "url": "/chichi/static/js/2.c4d6de3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chichi/static/js/2.c4d6de3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b2ddda06fac4b0e841",
+    "url": "/chichi/static/js/main.d378a1b0.chunk.js"
+  },
+  {
+    "revision": "8bbde2f5f27150fd7599",
+    "url": "/chichi/static/js/runtime-main.a49852f2.js"
+  }
+]);

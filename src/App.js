2f03e282b654d8955a82debeea65df71631fdf10
panikite <ne.isnuje@gmail.com>
2020-09-11 00:19:06 +0300

    create React App

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
-      <p>Hello Perkosrak!</p>
-      <p>Hello All!</p>
+      <p>Hello World of Cats and Dogs!</p>
     </div>
   );
 }

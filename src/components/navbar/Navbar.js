0b2b0fca693517b2cb6bd112cc6280a4bd56fd29
panikite <ne.isnuje@gmail.com>
2020-09-11 16:08:56 +0300

    add NavLink and NavLink basic styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../../img/logo.png';
 import './navbar.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -8,9 +9,9 @@ const Navbar = () => {
         <nav className = "navbar">
             <img src={Logo} className = "navbar_logo" alt="app_logo"/>
             <ul>
-                <li> <a href="/main"> Home </a></li>
-                <li> <a href="/categories"> Categores </a></li>
-                <li> <a href="/charts"> Charts </a></li>
+                <li> <NavLink to="/main" activeClassName="active" > Home </NavLink></li>
+                <li> <NavLink to="/categories" activeClassName="active" > Categores </NavLink></li>
+                <li> <NavLink to="/charts" activeClassName="active"> Charts </NavLink></li>
             </ul>
         </nav>
     )
